72835562cff0ce7b619e9125fdd6df152af5aefd
Lea Lamatina <lea.lamatina@gmail.com>
2018-07-17 16:37:12 -0500

    Adding the Final product of PyPoll.py to the repo

--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -6,47 +6,40 @@ VotingDataRaw = 'C:/Users/leala/Documents/Data Git Repo/python-challenge/Python_
 #create a list of Candidates
 Candidates = ["Correy", "Khan", "Li", "O'Tooley"]
 
-#and then variables to store the vote count for each Candidate
+#So many variables to define
 Can1 = 0
 Can2 = 0
 Can3 = 0
 Can4 = 0
+Voters = 0
 
 #get to reading this document aka Alex. Alex? Because why not
 with open(VotingDataRaw, "r") as Alex:
     voteData = csv.reader(Alex)
     next(voteData)
     
-#loop throught and find total number of votes cast
-    Voters = 0
     for row in voteData:
+#loop throught and find total number of votes cast
         Voters = Voters + 1
-
-#Loop through and grab vote count for each Candidate
-    for lines in voteData:
-        if line[3] == Candidates[0]:
+#grab vote count for each Candidate whilst looping
+        if row[2] == Candidates[0]:
             Can1 = Can1 + 1
-        elif line[3] == Candidates[1]:
+        elif row[2] == Candidates[1]:
             Can2 = Can2 + 1
-        elif line[3] == Candidates[2]:
-            Can3 = Can3 +1
+        elif row[2] == Candidates[2]:
+            Can3 = Can3 + 1
         else:
-            Can4 = Can4 +1
-    
-    print(Can1)
-    print(Can2)
-    print(Can3)
-    print(Can4)
+            Can4 = Can4 + 1
 
 print("Election Results")
 print("--------------------------")
 print("Total Votes Cast: ", Voters)
 print("--------------------------")
-print("Candidate Totals:")
-print(Candidates[0], ": ")
-print(Candidates[1], ": ")
-print(Candidates[2], ": ")
-print(Candidates[3], ": ")
+print("- Candidate Totals -")
+print(Candidates[0], ">> ", Can1, ":", "{0:.0%}".format(Can1/Voters))
+print(Candidates[1], ">> ", Can2, ":", "{0:.0%}".format(Can2/Voters))
+print(Candidates[2], ">> ", Can3, ":", "{0:.0%}".format(Can3/Voters))
+print(Candidates[3], ">> ", Can4, ":", "{0:.0%}".format(Can4/Voters))
 print("--------------------------")
-print("Winner with ", " of the vote, ")
+print("Winner: Khan") # dictionary return key value?
 print("--------------------------")
