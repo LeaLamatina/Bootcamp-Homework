f45311adfa0d406205690cb30b7a28452f8084da
Lea Lamatina <lea.lamatina@gmail.com>
2018-09-22 16:50:10 -0500

    cleaning
    better sketched out index.html, and a working version of scrape

--- Mission to Mars/scrape_mars.py
+++ Mission to Mars/scrape_mars.py
@@ -5,67 +5,50 @@ import pandas as pd
 import time
 
 def main():
-	nasa_web = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+	# variables for the urls to be scraped
+  nasa_web = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
 	jpl_web = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
 	nasa_twitter = "https://twitter.com/marswxreport?lang=en"
 	facts_web = "https://space-facts.com/mars/"
 	usgu_web = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
 
-	list_of_stuff =[nasa_web]
-
-	# Initiate headless driver for deployment
-	executable_path = {'executable_path': 'chromedriver.exe'}
+	# variables for commonly used actions
+  executable_path = {'executable_path': 'chromedriver.exe'}
 	browser = Browser('chrome', **executable_path, headless=False)
-	
-	for site in list_of_stuff:
-		a, b = mars_news(browser, site)
-		# todo: something with a (title)
-		# todo: somethin with b (paragraph)
 
-
-	# news_title, news_paragraph = mars_news(browser)
-
-	# Stop webdriver and return data
-	browser.quit()
-	return main
-
-
-def mars_news(browser, website):
-	browser.visit(website)
-
-	# title of first article
+  # --- MARS NEWS SCRAPE ---
+  browser.visit(website)
+	  # title of first article
 	nasa_title = (bs(browser.html, 'html.parser').find('div', class_="content_title")).text.strip()
-
-	# first paragraph of first article
+	  # first paragraph of first article
 	nasa_para = (bs(browser.html, 'html.parser').find('div', class_="article_teaser_body")).text.strip()
 
-	return nasa_title, nasa_para
-
-
-def space_image(browser):
-	browser.visit(jpl_web)
+  # --- JPL IMAGE SCRAPE ---
+  browser.visit(jpl_web)
 	browser.find_by_id("full_image").click()
-
-	# the browser tried to click too quickly so I had to add a sleep function
+	  # the browser tried to click too quickly so I had to add a sleep function
 	time.sleep(5)
 	browser.click_link_by_partial_text('more info')
-
+    # creating variables to hold the full url
 	jpl_end = (bs(browser.html, 'html.parser').find('img', class_='main_image')['src'])
-
 	full_jpl = (jpl_web + jpl_end)
 
-	return full_jpl
+  # --- MARS TWITTER SCRAPE --
+  browser.visit(nasa_twitter)
+	tweet = (bs(browser.html, 'html.parser').find('div', class_="js-tweet-text-container")).text.strip()
 
+  # --- MARS FACTS ---
+  mars_facts = pd.DataFrame(pd.read_html(facts_web)[0])
+	facts_html = mars_facts.to_html()
 
-def hemispheres(browser):
-	# empty list for the string
-	hemisphere_img = []
+  # --- MARS HEMISPHERE PICTURES ---
+  hemisphere_img = []
 
-	# open the page
+		# open the page
 	browser.visit(usgu_web)
 	links = browser.find_by_css("a.product-item h3")
 
-	# loop through, grab the title and url and store it in a dictionary
+		# loop through, grab the title and url and store it in a dictionary
 	for link in range(len(links)):
 		images= {}
 		browser.find_by_css("a.product-item h3")[link].click()
@@ -78,24 +61,9 @@ def hemispheres(browser):
 		hemisphere_img.append({"title": image_title, "image_url": image_url})
 		browser.back()
 
-	return hemisphere_img
+  mars_dictionary = {"nasa_title": nasa_title, "nasa_para": nasa_para, "full_jpl": full_jpl,
+                    "tweet": tweet, "facts_html": facts_html, "hemisphere_img": hemisphere_img}
+  browser.quit()
 
-
-def twitter_weather(browser):
-	browser.visit(nasa_twitter)
-	tweet = (bs(browser.html, 'html.parser').find('div', class_="js-tweet-text-container")).text.strip()
-
-	return tweet
-
-
-def mars_facts():
-	mars_facts = pd.DataFrame(pd.read_html(facts_web)[0])
-	facts_html = mars_facts.to_html()
-
-	return facts_html
-
-if __name__ == "__main__":
-
-	# If running as script, print scraped data
-	print(main())
-
+  #return a dictionary of all the information
+  return mars_dictionary
