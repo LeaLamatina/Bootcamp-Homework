753694ce1f557048afc9bbbe04a881516d640cb6
Lea Lamatina <lea.lamatina@gmail.com>
2018-09-24 20:42:56 -0500

    homework push

--- Mission to Mars/app.py
+++ Mission to Mars/app.py
@@ -5,20 +5,20 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
 mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-	mars_dictionary = mongo.db.mars_dictionary.find_one()
-	return render_template("index.html", mars_dictionary=mars_dictionary)
+  mars = mongo.db.collection.find()
+  return render_template("index.html", mars=mars)
 
 @app.route("/scrape")
 def scraper():
-	mars_dictionary = mongo.db.mars_dictionary
-	mars_data = scrape_mars.scrape()
-	mars_dictionary.update({}, mars_data, upsert=True)
+  mars = mongo.db.mars
+  mars_data = scrape_mars.scrape()
+  mars.update({}, mars_dictionary, upsert=True)
   return redirect("/", code=302)
 
 if __name__ == "__main__":
-	app.run(debug=True)
+  app.run(debug=True)
--- Mission to Mars/scrape_mars.py
+++ Mission to Mars/scrape_mars.py
@@ -5,28 +5,26 @@ import pandas as pd
 import time
 
 def scrape():
-	# variables for the urls to be scraped
+  executable_path = {'executable_path': 'chromedriver.exe'}
+  browser = Browser('chrome', **executable_path, headless=False)
+
   nasa_web = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
   jpl_web = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
   nasa_twitter = "https://twitter.com/marswxreport?lang=en"
   facts_web = "https://space-facts.com/mars/"
   usgu_web = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
 
-	# variables for commonly used actions
-  executable_path = {'executable_path': 'chromedriver.exe'}
-  browser = Browser('chrome', **executable_path, headless=False)
-
   # --- MARS NEWS SCRAPE ---
   browser.visit(nasa_web)
-	  # title of first article
+    # title of first article
   nasa_title = (bs(browser.html, 'html.parser').find('div', class_="content_title")).text.strip()
-	  # first paragraph of first article
+    # first paragraph of first article
   nasa_para = (bs(browser.html, 'html.parser').find('div', class_="article_teaser_body")).text.strip()
 
   # --- JPL IMAGE SCRAPE ---
   browser.visit(jpl_web)
   browser.find_by_id("full_image").click()
-	  # the browser tried to click too quickly so I had to add a sleep function
+    # the browser tried to click too quickly so I had to add a sleep function
   time.sleep(5)
   browser.click_link_by_partial_text('more info')
     # creating variables to hold the full url
@@ -44,28 +42,26 @@ def scrape():
   # --- MARS HEMISPHERE PICTURES ---
   hemisphere_img = []
 
-		# open the page
+    # open the page
   browser.visit(usgu_web)
   links = browser.find_by_css("a.product-item h3")
 
-		# loop through, grab the title and url and store it in a dictionary
+    # loop through, grab the title and url and store it in a dictionary
   for link in range(len(links)):
-  	images= {}
-  	browser.find_by_css("a.product-item h3")[link].click()
-  	time.sleep(5)
-  	image_url = browser.find_link_by_text('Sample')
-  	images['img_url'] = image_url['href']
-  	browser.find_by_css('h2.title').text
-  	image_title = browser.find_by_css('h2.title').text
-  	images['title'] = image_title
-  	hemisphere_img.append({"title": image_title, "image_url": image_url})
-  	browser.back()
+    images= {}
+    browser.find_by_css("a.product-item h3")[link].click()
+    time.sleep(5)
+    image_url = browser.find_link_by_text('Sample')
+    images['img_url'] = image_url['href']
+    browser.find_by_css('h2.title').text
+    image_title = browser.find_by_css('h2.title').text
+    images['title'] = image_title
+    hemisphere_img.append({"title": image_title, "image_url": image_url})
+    browser.back()
 
   mars_dictionary = {"nasa_title": nasa_title, "nasa_para": nasa_para, "full_jpl": full_jpl, "tweet": tweet, "facts_html": facts_html, "hemisphere_img": hemisphere_img}
 
-  # print(mars_dictionary)
-
   browser.quit()
 
-  #return a dictionary of all the information
+  print(mars_dictionary)
   return mars_dictionary
